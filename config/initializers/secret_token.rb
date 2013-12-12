702437cbcff393fe2fab2890c033531caf6c5722
Przybylski 중광 <r351574nc3@gmail.com>
2013-12-12 14:33:15 -0700

    Adding rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CommunityshareWeb::Application.config.secret_key_base = 'f2452d0f89c550ac60b4669c26ec08f2d66251563bc31b15f913cf689f8f353f37b3812c018acf5eab9f40d49354a886635c5b83144d5d16cac673ee918fa98e'
