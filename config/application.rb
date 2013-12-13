387ae6bed08377b8b8eddf94d69f21abaf39b674
Przybylski 중광 <r351574nc3@gmail.com>
2013-12-13 11:29:19 -0700

    Updating sass paths

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module CommunityshareWeb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.enabled = true
+    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
