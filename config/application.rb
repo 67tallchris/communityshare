066ba2812d4b67a2a9efb97a061c261204d60f7b
Przybylski 중광 <r351574nc3@gmail.com>
2013-12-13 11:39:01 -0700

    Adding stuff to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module CommunityshareWeb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.enabled = true
-    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
+    # config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
+    config.assets.paths << Rails.root.join("vendor", "assets", "javascripts").to_s
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
